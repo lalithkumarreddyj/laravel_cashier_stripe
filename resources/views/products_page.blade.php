624565edbb9dfa6c4aa1941a881a811319554365
lalithkumarreddyj <51774573+lalithkumarreddyj@users.noreply.github.com>
2022-07-25 00:08:10 +0530

    added-images location
    added-images location

--- resources/views/products_page.blade.php
+++ resources/views/products_page.blade.php
@@ -44,7 +44,7 @@
             @foreach($products_list as $products_key => $product_array)
             <div class="item">
                 <h3 class="center">{{$product_array['name']}}</h3>
-                <img src="http://localhost/stripe-tasks/stripe-charge/public/storage/{{$product_array['image_name']}}" alt="No_image" style="margin-left:20%">
+                <img src="http://localhost/laravel_cashier_stripe/public/storage/{{$product_array['image_name']}}" alt="No_image" style="margin-left:20%">
                 <h3 style="float:left;width:100%">Item Price : <span style="color:green">INR {{$product_array['price']}}</span></h3>
                 <h3>Proceed to buy : <button><a href="{{url('/product_specifications').'/'.encrypt($product_array['id'])}}">Buy Now</a></button></h3>
             </div>
